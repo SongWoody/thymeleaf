1b0e71a2cafafd8ffd3e2cfeab807e56a37ee418
Woody <syg1138@dayside.co.kr>
2025-07-23 09:31:11 +0900

    리스트 형태의 데이터 노출 연습

--- src/main/kotlin/com/thyme/example/thymeleaf_practice/ContentController.kt
+++ src/main/kotlin/com/thyme/example/thymeleaf_practice/ContentController.kt
@@ -1,5 +1,6 @@
 package com.thyme.example.thymeleaf_practice
 
+import com.thyme.example.thymeleaf_practice.medel.User
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,5 +19,17 @@ class ContentController {
     fun hello(model: Model): String {
         model.addAttribute("message", "Hello, My name is Mr.Song")
         return "hello"
+    }
+
+    @GetMapping("user")
+    fun user(model: Model): String {
+        val userList = listOf<User>(
+            User(1, "Woody", "syg1138@gmail.com"),
+            User(2, "Bruno", "bruno8@gmail.com"),
+            User(3, "Laura", "laura@gmail.com")
+        )
+        model.addAttribute("userList", userList)
+
+        return "user-list"
     }
 }
---
+++ src/main/kotlin/com/thyme/example/thymeleaf_practice/medel/User.kt
@@ -0,0 +1,7 @@
+package com.thyme.example.thymeleaf_practice.medel
+
+data class User(
+    val id: Long,
+    val name: String,
+    val email: String,
+)
